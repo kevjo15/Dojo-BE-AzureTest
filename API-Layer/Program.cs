7fb3c407259f73e3a53ef7207450e37fd85fbfe3
Nemanja1208 <nevaljao994@gmail.com>
2024-02-29 22:46:55 +0100

    Added dummy code for CI/CD purposes

--- API-Layer/Program.cs
+++ API-Layer/Program.cs
@@ -15,11 +15,8 @@ builder.Services.AddInfrastructureLayer();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
