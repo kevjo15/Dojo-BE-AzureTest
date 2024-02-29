4b258db100a34db081667822c101a053a3bf1696
Nemanja1208 <nevaljao994@gmail.com>
2024-02-29 20:59:40 +0100

    Initialize Project Setup Clean Architecture

---
+++ API-Layer/Program.cs
@@ -0,0 +1,30 @@
+using Application_Layer;
+using Infrastructure_Layer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+//  Add our own services (layers) here
+builder.Services.AddApplicationLayer();
+builder.Services.AddInfrastructureLayer();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Infrastructure-Layer/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure_Layer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
