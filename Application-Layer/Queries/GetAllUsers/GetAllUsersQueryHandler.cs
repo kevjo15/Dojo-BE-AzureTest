6ba46c0df91652dde450300e5083ba3d6d1e27e9
Elliot Dahlin <115067290+ElliotDahlin@users.noreply.github.com>
2024-03-11 23:36:51 +0100

    I have added GetAllUserQuery and Handler (#70)
    Co-authored-by: ElliotRespository <115067290+ElliotRespository@users.noreply.github.com>

---
+++ Application-Layer/Queries/GetAllUsers/GetAllUsersQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Domain_Layer.Models.UserModel;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application_Layer.Queries.GetAllUsers
+{
+    public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, IEnumerable<UserModel>>
+    {
+        private readonly UserManager<UserModel> _userManager;
+
+        public GetAllUsersQueryHandler(UserManager<UserModel> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IEnumerable<UserModel>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            return await _userManager.Users.ToListAsync(cancellationToken);
+        }
+    }
+}
