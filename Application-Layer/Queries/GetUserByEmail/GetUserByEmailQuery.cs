38d3f3b25865faa53b31252243ed058248a22094
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2024-03-19 16:20:31 +0100

    #28 user managment get user by email query+handler (#85)
    * Added GetUserByEmailQuery and Handler
    
    * Added GetUserByEmailQuery and Handler
    
    * Changed out manager to UserRepository. Also changed language in my error handling
    
    * Dotnet format
    
    * fixed error
    
    * Added GetUserByEmailQuery and Handler
    
    * Changed out manager to UserRepository. Also changed language in my error handling
    
    * fixat merge conflicts
    
    * changed GetUserByEmailQuery and Handler so it uses UserModel directly. insted of UserIdentity
    
    ---------
    
    Co-authored-by: Nemanja1208 <nevaljao994@gmail.com>

---
+++ Application-Layer/Queries/GetUserByEmail/GetUserByEmailQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain_Layer.Models.UserModel;
+using MediatR;
+
+namespace Application_Layer.Queries.GetUserByEmail
+{
+    public class GetUserByEmailQuery : IRequest<UserModel>
+    {
+        public string Email { get; private set; }
+        public GetUserByEmailQuery(string email)
+        {
+            Email = email;
+        }
+    }
+}
---
+++ Application-Layer/Queries/GetUserByEmail/GetUserByEmailQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using Domain_Layer.Models.UserModel;
+using Infrastructure_Layer.Repositories.User;
+using MediatR;
+
+namespace Application_Layer.Queries.GetUserByEmail
+{
+    public class GetUserByEmailQueryHandler : IRequestHandler<GetUserByEmailQuery, UserModel>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserByEmailQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserModel> Handle(GetUserByEmailQuery request, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(request.Email))
+            {
+                throw new ArgumentException("Email cannot be empty!");
+            }
+
+            try
+            {
+                var user = await _userRepository.GetUserByEmailAsync(request.Email);
+                if (user == null)
+                {
+                    throw new KeyNotFoundException($"User with Email '{request.Email}' cannot be found!.");
+                }
+
+                return user;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
