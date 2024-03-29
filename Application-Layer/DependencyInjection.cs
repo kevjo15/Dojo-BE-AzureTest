3530837184db2406a68d5f1bac2a59bd341a972f
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-03-29 15:24:07 +0100

    LoggingBehaviour is created and services are added in to DI

--- Application-Layer/DependencyInjection.cs
+++ Application-Layer/DependencyInjection.cs
@@ -12,16 +12,22 @@ namespace Application_Layer
         public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
         {
             var assembly = typeof(DependencyInjection).Assembly;
-            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly));
+            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(assembly))
+                 .AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>))
+                 .AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
 
             services.AddValidatorsFromAssembly(assembly);
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+
 
             var config = new MapperConfiguration(cfg =>
             {
                 cfg.AddProfile<MappingProfiles>();
             });
             services.AddSingleton(config.CreateMapper());
+           
+   
 
             return services;
         }
---
+++ Application-Layer/PipelineBehaviour/LoggingBehaviour.cs
@@ -0,0 +1,68 @@
+﻿
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace Application_Layer.PipelineBehaviour
+{
+    //public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    //{
+    //    private readonly ILogger<LoggingBehaviour<TRequest, TResponse>> _logger;
+
+    //    public LoggingBehaviour(ILogger<LoggingBehaviour<TRequest, TResponse>> logger)
+    //    {
+    //        _logger = logger;
+    //    }
+
+    //    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    //    {
+    //        // Log request type
+    //        _logger.LogInformation($"Handling {typeof(TRequest).Name}");
+
+    //        // Log request properties, excluding sensitive ones
+    //        Type myType = request.GetType();
+    //        IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+    //        foreach (PropertyInfo prop in props)
+    //        {
+    //            // Check if the property has the [JsonIgnore] attribute
+    //            if (!prop.GetCustomAttributes(typeof(JsonIgnoreAttribute), true).Any())
+    //            {
+    //                object propValue = prop.GetValue(request, null)!;
+    //                _logger.LogInformation("{Property} : {@Value}", prop.Name, propValue);
+    //            }
+    //        }
+
+    //        var response = await next();
+
+    //        // Log response type
+    //        _logger.LogInformation($"Handled {typeof(TResponse).Name}");
+
+    //        return response;
+    //    }
+    //}
+    public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingBehaviour<TRequest, TResponse>> _logger;
+        public LoggingBehaviour(ILogger<LoggingBehaviour<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            //Request
+            _logger.LogInformation($"Handling {typeof(TRequest).Name}");
+            Type myType = request.GetType();
+            IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+            foreach (PropertyInfo prop in props)
+            {
+                object propValue = prop.GetValue(request, null)!;
+                _logger.LogInformation("{Property} : {@Value}", prop.Name, propValue);
+            }
+            var response = await next();
+            //Response
+            _logger.LogInformation($"Handled {typeof(TResponse).Name}");
+            return response;
+        }
+    }
+}
