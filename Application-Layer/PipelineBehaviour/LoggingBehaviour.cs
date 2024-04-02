68d3522e8f4950485ed655781804a1257d4095b0
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-04-02 11:34:26 +0200

    Using statement and comments are removed

--- Application-Layer/PipelineBehaviour/LoggingBehaviour.cs
+++ Application-Layer/PipelineBehaviour/LoggingBehaviour.cs
@@ -1,46 +1,10 @@
 ﻿
 using System.Reflection;
-using System.Text.Json.Serialization;
 using MediatR;
 using Microsoft.Extensions.Logging;
 
 namespace Application_Layer.PipelineBehaviour
 {
-    //public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-    //{
-    //    private readonly ILogger<LoggingBehaviour<TRequest, TResponse>> _logger;
-
-    //    public LoggingBehaviour(ILogger<LoggingBehaviour<TRequest, TResponse>> logger)
-    //    {
-    //        _logger = logger;
-    //    }
-
-    //    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
-    //    {
-    //        // Log request type
-    //        _logger.LogInformation($"Handling {typeof(TRequest).Name}");
-
-    //        // Log request properties, excluding sensitive ones
-    //        Type myType = request.GetType();
-    //        IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
-    //        foreach (PropertyInfo prop in props)
-    //        {
-    //            // Check if the property has the [JsonIgnore] attribute
-    //            if (!prop.GetCustomAttributes(typeof(JsonIgnoreAttribute), true).Any())
-    //            {
-    //                object propValue = prop.GetValue(request, null)!;
-    //                _logger.LogInformation("{Property} : {@Value}", prop.Name, propValue);
-    //            }
-    //        }
-
-    //        var response = await next();
-
-    //        // Log response type
-    //        _logger.LogInformation($"Handled {typeof(TResponse).Name}");
-
-    //        return response;
-    //    }
-    //}
     public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
     {
         private readonly ILogger<LoggingBehaviour<TRequest, TResponse>> _logger;
