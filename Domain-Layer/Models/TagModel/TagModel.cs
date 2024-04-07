63bfd95c2e9951f86e5d716148dc01d48e8b5e77
BojanMirkovic <115114379+BojanMirkovic@users.noreply.github.com>
2024-04-07 22:00:59 +0200

    #30 course management create models (#112)
    * CourseModel class is created
    
    * TagModel and CourseHasTag are created

---
+++ Domain-Layer/Models/TagModel/TagModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Domain_Layer.Models.TagModel
+{
+    public class TagModel
+    {
+        public string TagId { get; set; } = Guid.NewGuid().ToString();
+        public string TagName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
