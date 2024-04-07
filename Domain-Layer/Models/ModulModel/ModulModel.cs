12cebde95da149cdc479d043840ac99db4f4e1be
Bojan Mirkovic <bojan.mirkovic@student.nbi-handelsakademin.se>
2024-04-07 12:13:26 +0200

    ContentModel is created

--- Domain-Layer/Models/ModulModel/ModulModel.cs
+++ Domain-Layer/Models/ModulModel/ModulModel.cs
@@ -5,7 +5,7 @@ namespace Domain_Layer.Models.ModulModel
     {
         public string ModulId { get; set; } = Guid.NewGuid().ToString();
         public string CourseId { get; set; } = string.Empty;
-        public string Title { get; set; } = string.Empty;
+        public string ModulTitle { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public int OrderInCourse { get; set; }
         public string? ResourceURL { get; set; }
